c0c390d551b2bc3c2905f22868ff7e5632292341
GITwonwoo <kaneira2178@naver.com>
2024-04-02 18:22:24 +0900

    2024.04.02
    q

--- js/API_sub.js
+++ js/API_sub.js
@@ -1,25 +1,5 @@
-// -----------------함께 구매한 상품-------------------
-let together = ["역사", "기욤뮈소", "박완서", "경제", "한국사", "파리", "여행", "음식","자동차","스포츠"];
-
-for (let i = 0; i < together.length; i++) {
-
-    $.ajax({
-        method: "GET",
-        url: "https:dapi.kakao.com/v3/search/book?target=title",
-        data: { query: together[i] },
-        headers: { Authorization: "KakaoAK 7fd50bb6d443accb5e07119127aae332" }
-    })
-        .done(function (msg) {
-            const divs = $(".sld .sldimg");
-            divs.eq(i).prepend("<img src='" + msg.documents[0].thumbnail + "'/>");
-              
-            divs.eq(i).append("<h4>" + msg.documents[0].title + "</h4>");
-            divs.eq(i).append("<h6>" + msg.documents[0].authors + "</h6>");
- 
-        });
-}
-// -----------------함께 구매한 상품-------------------
-let book = ["자기개발"];
+// ----------메인 책정보
+let book = ["일류의 조건"];
 
 for (let i = 0; i < book.length; i++) {
 
@@ -42,4 +22,26 @@ for (let i = 0; i < book.length; i++) {
  
         });
 }
+
+// -----------------함께 구매한 상품-------------------
+let together = ["역사", "기욤뮈소", "박완서", "경제", "한국사", "파리", "여행", "음식","자동차","스포츠"];
+
+for (let i = 0; i < together.length; i++) {
+
+    $.ajax({
+        method: "GET",
+        url: "https:dapi.kakao.com/v3/search/book?target=title",
+        data: { query: together[i] },
+        headers: { Authorization: "KakaoAK 7fd50bb6d443accb5e07119127aae332" }
+    })
+        .done(function (msg) {
+            const divs = $(".sld .sldimg");
+            divs.eq(i).prepend("<img src='" + msg.documents[0].thumbnail + "'/>");
+              
+            divs.eq(i).append("<h5>" + msg.documents[0].title + "</h5>");
+            divs.eq(i).append("<h6>" + msg.documents[0].authors + "</h6>");
+ 
+        });
+}
+
 
